fd439f07336d9bd4e7a24f2ac8e7ae6fee0e6940
Kirsi Kauppinen <kirsiniina@gmail.com>
2023-07-20 12:36:28 +0300

    4.5: Add method to find favorite blog

--- Osa4/utils/list_helpers.js
+++ Osa4/utils/list_helpers.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes);
+  return sortedBlogs[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
