e830fd29e89c99f82f8ec7b650a38c30af6cee64
Kirsi Kauppinen <kirsiniina@gmail.com>
2023-07-21 21:01:32 +0300

    4.15: Add possibility to add users

--- Osa4/app.js
+++ Osa4/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 
 logger.info('connecting to database...');
@@ -22,6 +23,7 @@ mongoose.connect(MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
---
+++ Osa4/tests/users_api.test.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const bcrypt = require('bcrypt');
+const app = require('../app');
+
+const api = supertest(app);
+
+const User = require('../models/user');
+const helper = require('./utils/test_helper');
+
+beforeEach(async () => {
+  await User.deleteMany({});
+
+  const passwordHash = await bcrypt.hash('password', 10);
+  const user = new User({ username: 'root', passwordHash });
+
+  await user.save();
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+describe('GET /users', () => {
+  test('returns all existing users', async () => {
+    const response = await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.length).toBe(1);
+  });
+});
+
+describe('POST /users', () => {
+  test('can create new user', async () => {
+    const existingUsers = await helper.usersInDb();
+    const newUser = {
+      name: 'Simon',
+      username: 'theCat',
+      password: 'drowssap',
+    };
+
+    const response = await api.post('/api/users').send(newUser).expect(201);
+    const allUsers = await helper.usersInDb();
+
+    expect(response.body.password).not.toBeDefined();
+    expect(allUsers).toHaveLength(existingUsers.length + 1);
+
+    const usernames = allUsers.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
