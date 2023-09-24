571de3a8175727424f177220ed348686469eb4a2
Kirsi Kauppinen <kirsiniina@gmail.com>
2023-09-24 14:35:21 +0300

    5.7: Show all details of blog

--- Osa5/bloglist-frontend/src/components/Blog.js
+++ Osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShow = () => {
+    setShowAll(!showAll);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button type="button" onClick={toggleShow}>
+        {showAll ? 'Hide' : 'View'}
+      </button>
+      {showAll && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} <button>Like!</button>
+          </div>
+          <div>{blog.user.username}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
